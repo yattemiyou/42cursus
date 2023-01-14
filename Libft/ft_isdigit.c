69ef02e43109376adf394030812da3ef1c5cc40d
Your Name <you@example.com>
2023-01-15 08:37:43 +0900

    ft_isdigit

--- Libft/ft_isdigit.c
+++ Libft/ft_isdigit.c
@@ -1,19 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/14 12:05:49 by anonymous         #+#    #+#             */
-/*   Updated: 2023/01/15 08:33:35 by anonymous        ###   ########.fr       */
+/*   Created: 2023/01/15 08:28:09 by anonymous         #+#    #+#             */
+/*   Updated: 2023/01/15 08:34:39 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-
-#endif
+int	ft_isdigit(int c)
+{
+	return ('0' <= c && c <= '9');
+}
