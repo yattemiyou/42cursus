837af24053a3ccebf2e8feab6b800170c5590193
Your Name <you@example.com>
2023-01-15 08:11:39 +0900

    Libft

--- Libft/ft_isalpha.c
+++ Libft/ft_isalpha.c
@@ -6,11 +6,11 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 12:05:25 by anonymous         #+#    #+#             */
-/*   Updated: 2023/01/14 13:31:06 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/01/15 08:09:47 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isalpha(int c)
 {
-	return (0);
+	return (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'));
 }
