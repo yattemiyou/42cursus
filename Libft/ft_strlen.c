0357b86123fa825462318ea86822de6f12208bca
Your Name <you@example.com>
2023-01-15 11:02:37 +0900

    ft_strlen

--- Libft/ft_strlen.c
+++ Libft/ft_strlen.c
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/14 12:05:49 by anonymous         #+#    #+#             */
-/*   Updated: 2023/01/15 10:39:24 by anonymous        ###   ########.fr       */
+/*   Created: 2023/01/15 10:40:04 by anonymous         #+#    #+#             */
+/*   Updated: 2023/01/15 11:00:45 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_strlen(const char *s);
+int	ft_strlen(const char *s)
+{
+	const char	*ptr;
 
-#endif
+	ptr = s;
+	while (*ptr)
+		ptr++;
+	return (ptr - s);
+}
