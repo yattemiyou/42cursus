8d69a8be1528c56fd4fdac0bcfa5c049c0d36100
Your Name <you@example.com>
2023-01-15 12:20:14 +0900

    ft_memset

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -1,26 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/14 12:05:49 by anonymous         #+#    #+#             */
-/*   Updated: 2023/01/15 11:53:28 by anonymous        ###   ########.fr       */
+/*   Created: 2023/01/15 11:36:01 by anonymous         #+#    #+#             */
+/*   Updated: 2023/01/15 12:18:38 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stddef.h>
-
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *s, int c, size_t n);
-
-#endif
+void	*ft_memset(void *s, int c, size_t n)
+{
+	if (s == NULL)
+		return (s);
+	while (n)
+		((char *)s)[--n] = c;
+	return (s);
+}
