14392275f61c79062272bad0a99b37862ede6ea8
Your Name <you@example.com>
2023-01-15 11:31:48 +0900

    ft_strlen

--- Libft/ft_strlen.c
+++ Libft/ft_strlen.c
@@ -6,13 +6,13 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 10:40:04 by anonymous         #+#    #+#             */
-/*   Updated: 2023/01/15 11:00:45 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/01/15 11:27:53 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
 	const char	*ptr;
 
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,18 +6,20 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 12:05:49 by anonymous         #+#    #+#             */
-/*   Updated: 2023/01/15 10:39:24 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/01/15 11:29:53 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_strlen(const char *s);
+# include <stddef.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
 
 #endif
