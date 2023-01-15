e2a3dedcae40b86c8561d178401e706b56e15454
Your Name <you@example.com>
2023-01-15 09:40:19 +0900

    ft_isascii

--- Libft/ft_isascii.c
+++ Libft/ft_isascii.c
@@ -1,21 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/14 12:05:49 by anonymous         #+#    #+#             */
-/*   Updated: 2023/01/15 09:16:33 by anonymous        ###   ########.fr       */
+/*   Created: 2023/01/15 08:52:33 by anonymous         #+#    #+#             */
+/*   Updated: 2023/01/15 09:14:03 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-
-#endif
+int	ft_isascii(int c)
+{
+	return (0 <= c && c <= 127);
+}
