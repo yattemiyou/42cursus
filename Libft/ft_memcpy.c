6be5c779a6a8ab75240f2a6529dd85e84505308d
Your Name <you@example.com>
2023-01-15 15:31:45 +0900

    ft_memcpy

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/14 12:05:49 by anonymous         #+#    #+#             */
-/*   Updated: 2023/01/15 15:28:46 by anonymous        ###   ########.fr       */
+/*   Created: 2023/01/15 14:18:05 by anonymous         #+#    #+#             */
+/*   Updated: 2023/01/15 15:30:41 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stddef.h>
-
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-
-#endif
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	while (n--)
+	{
+		((char *)dest)[n] = ((char *)src)[n];
+	}
+	return (dest);
+}
