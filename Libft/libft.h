19d4009ee98fff073679c2bca5b7a96d83cd86f0
Your Name <you@example.com>
2023-01-16 20:22:44 +0900

    ft_tolower

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 12:05:49 by anonymous         #+#    #+#             */
-/*   Updated: 2023/01/16 20:09:45 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/01/16 20:19:19 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
