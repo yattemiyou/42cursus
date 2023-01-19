5fa62296b7f9371cd392b806ff64a19122c8c4a4
Your Name <you@example.com>
2023-01-19 20:55:39 +0900

    ft_strchr

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 20:22:22 by anonymous         #+#    #+#             */
-/*   Updated: 2023/01/19 20:44:00 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/01/19 20:55:21 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@ char	*ft_strchr(const char *s, int c)
 {
 	if (c == '\0')
 		return ((char *)s + ft_strlen(s));
-	while (*s++ != '\0')
+	while (*s != '\0')
 	{
 		if (*s == (char)c)
 			return ((char *)s);
+		s++;
 	}
 	return (NULL);
 }
