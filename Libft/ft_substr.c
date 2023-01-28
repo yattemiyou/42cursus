1989a37a9ffe21b7aa2b056335e97d934a397023
Your Name <you@example.com>
2023-01-28 15:36:40 +0900

    ft_substr

---
+++ Libft/ft_substr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/28 14:10:16 by anonymous         #+#    #+#             */
+/*   Updated: 2023/01/28 15:35:48 by anonymous        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	size;
+	void	*buffer;
+
+	if (s == NULL)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_calloc(1, 1));
+	size = ft_strlen(s) - start;
+	if (len < size)
+		size = len;
+	size += 1;
+	buffer = ft_calloc(1, size);
+	ft_strlcpy(buffer, s + start, size);
+	return (buffer);
+}
