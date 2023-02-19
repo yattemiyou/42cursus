2274506321d6173a7e403abf075fb471213416f9
Your Name <you@example.com>
2023-02-19 17:11:03 +0900

    ft_atoi

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 13:05:21 by anonymous         #+#    #+#             */
-/*   Updated: 2023/01/22 11:31:00 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/02/19 17:10:17 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,6 @@ int	ft_atoi(const char *nptr)
 		nptr++;
 	}
 	while (ft_isdigit(*nptr))
-	{
-		ret *= 10;
-		ret += (*nptr++ - '0');
-	}
+		ret = ret * 10 + (*nptr++ - '0');
 	return (sign * ret);
 }
