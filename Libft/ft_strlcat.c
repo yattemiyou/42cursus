889f2680d1d99e998a771e76df36454c9cd7a7ab
Your Name <you@example.com>
2023-02-19 15:18:59 +0900

    ft_strlcat

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 21:28:17 by anonymous         #+#    #+#             */
-/*   Updated: 2023/01/18 22:17:43 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/02/19 15:18:38 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t	offset;
 
+	if (dest == NULL)
+		return (ft_strlen(dest));
 	offset = ft_strlen(dest);
 	if (size <= offset)
 		return (size + ft_strlen(src));
