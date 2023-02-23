8b6494898e3066d1638d4a73415daf27d80b957c
Your Name <you@example.com>
2023-02-23 20:22:54 +0900

    ft_putstr_fd

--- Libft/ft_putstr_fd.c
+++ Libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 14:23:45 by anonymous         #+#    #+#             */
-/*   Updated: 2023/02/04 14:27:43 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/02/23 20:22:26 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
+	if (s == NULL)
+		return ;
 	write(fd, s, ft_strlen(s));
 }
