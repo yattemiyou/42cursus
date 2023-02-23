31bcf2802327b42fb5123646ea383ffd95c9ca3e
Your Name <you@example.com>
2023-02-23 19:01:11 +0900

    ft_calloc

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 11:36:14 by anonymous         #+#    #+#             */
-/*   Updated: 2023/02/19 16:24:34 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/02/23 18:59:16 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	if (nmemb > ULLONG_MAX / size)
 		return (NULL);
 	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
 	ft_memset(ptr, 0, nmemb * size);
 	return (ptr);
 }
