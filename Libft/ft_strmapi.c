2d4be6be7e858cd51e57cc5231e4d1b5ee2d27b5
Your Name <you@example.com>
2023-02-23 20:34:02 +0900

    ft_strmapi

--- Libft/ft_strmapi.c
+++ Libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 20:59:15 by anonymous         #+#    #+#             */
-/*   Updated: 2023/02/06 21:18:11 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/02/23 20:33:29 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,11 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char			*str;
 	unsigned int	index;
 
+	if (s == NULL)
+		return (NULL);
 	str = ft_strdup(s);
+	if (str == NULL)
+		return (NULL);
 	index = 0;
 	while (str[index])
 	{
