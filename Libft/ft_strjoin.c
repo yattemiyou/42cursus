d8b4ac3ed10e7ff2741177e1014b463edf923979
Your Name <you@example.com>
2023-02-24 22:16:40 +0900

    ft_strjoin

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 15:39:51 by anonymous         #+#    #+#             */
-/*   Updated: 2023/01/28 15:58:28 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/02/24 22:15:50 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	size = ft_strlen(s1) + ft_strlen(s2) + 1;
 	buffer = ft_calloc(1, size);
+	if (buffer == NULL)
+		return (NULL);
 	ft_strlcpy(buffer, s1, size);
 	ft_strlcat(buffer, s2, size);
 	return (buffer);
