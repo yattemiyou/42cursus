17adbda0a0037976f63a8b4491f5a88d4936332e
Your Name <you@example.com>
2023-02-25 15:38:58 +0900

    ft_striteri

--- Libft/ft_striteri.c
+++ Libft/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 21:20:44 by anonymous         #+#    #+#             */
-/*   Updated: 2023/02/06 21:27:14 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/02/25 15:38:27 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	index;
 
+	if (s == NULL)
+		return ;
 	index = 0;
 	while (s[index])
 	{
