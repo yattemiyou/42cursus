f59269c576cf7fa33a9172df615ec1cac8550857
Your Name <you@example.com>
2023-02-25 14:09:55 +0900

    ft_strtrim

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 16:00:00 by anonymous         #+#    #+#             */
-/*   Updated: 2023/02/18 17:55:48 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/02/25 14:09:01 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char		*start;
 	char		*end;
 
+	if (s1 == NULL)
+		return (NULL);
 	while (*ptr && ft_strchr(set, *ptr) != NULL)
 		ptr++;
 	start = (char *)ptr;
