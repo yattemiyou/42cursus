356c70983e58abfae3f014f9fdf27ba44697159b
Your Name <you@example.com>
2023-03-11 11:23:23 +0900

    ft_memcpy

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 14:18:05 by anonymous         #+#    #+#             */
-/*   Updated: 2023/01/15 18:19:29 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/03/11 11:22:34 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	return (ft_memmove(dest, src, n));
+	if (dest == src)
+		return (dest);
+	while (n--)
+		((unsigned char *)dest)[n] = ((const unsigned char *)src)[n];
+	return (dest);
 }
