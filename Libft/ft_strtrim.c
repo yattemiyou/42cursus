82b49d59378b8603c1c1d2ce0054440718e2fbce
Your Name <you@example.com>
2023-03-19 12:54:57 +0900

    ft_strtrim

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 16:00:00 by anonymous         #+#    #+#             */
-/*   Updated: 2023/02/26 18:15:30 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/03/19 12:51:52 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char const	*ptr = s1;
-	char		*start;
-	char		*end;
+	char const	*start;
+	char const	*end;
 
 	if (s1 == NULL)
 		return (NULL);
@@ -24,10 +24,12 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (ft_strdup(s1));
 	while (*ptr && ft_strchr(set, *ptr) != NULL)
 		ptr++;
-	start = (char *)ptr;
-	ptr = s1 + ft_strlen(s1);
-	while (ptr > start && ft_strchr(set, *ptr) != NULL)
-		ptr--;
-	end = (char *)++ptr;
+	start = ptr;
+	while (*ptr)
+	{
+		if (ft_strchr(set, *ptr) == NULL)
+			end = ptr + 1;
+		ptr++;
+	}
 	return (ft_substr(start, 0, end - start));
 }
