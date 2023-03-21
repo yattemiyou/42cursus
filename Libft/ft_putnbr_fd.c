fa80f5909ed9a4440c10bb84d617b11e7f1285ee
Your Name <you@example.com>
2023-03-21 12:57:43 +0900

    ft_putnbr_fd

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 14:35:33 by anonymous         #+#    #+#             */
-/*   Updated: 2023/02/05 09:32:16 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/03/21 12:56:24 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,15 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == INT_MIN)
-	{
-		ft_putstr_fd("-2147483648", fd);
-		return ;
-	}
+	unsigned int	positive;
+
+	positive = (unsigned int)n;
 	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		n *= -1;
+		positive = (unsigned int)(-1 * n);
 	}
-	if (n / 10 > 0)
-		ft_putnbr_fd(n / 10, fd);
-	ft_putchar_fd(n % 10 + '0', fd);
+	if (positive / 10 > 0)
+		ft_putnbr_fd(positive / 10, fd);
+	ft_putchar_fd(positive % 10 + '0', fd);
 }
