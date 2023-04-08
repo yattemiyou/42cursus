3fdfaa38eaf203fb3a7a40a6c3212d8f200d11cf
Your Name <you@example.com>
2023-04-08 14:29:35 +0900

    Born2beroot

--- Born2beroot/monitoring.sh
+++ Born2beroot/monitoring.sh
@@ -2,6 +2,8 @@
 
 LVM=`vgs --noheadings | awk '{if ($3 > 0) {print "yes"} else {print "no"}}'`
 
+USER=`who | wc -l`
+
 DEVICE="enp0s8"
 IP=`ip -4 addr show ${DEVICE} | grep inet | awk -F '[ /]' '{print $6}'`
 MAC=`ip link show ${DEVICE} | grep link | awk '{print $2}'`
@@ -9,5 +11,6 @@ MAC=`ip link show ${DEVICE} | grep link | awk '{print $2}'`
 NUMBER=$((36#`head -n 1 /var/log/sudo/seq`))
 
 echo "#LVM use: ${LVM}"
+echo "#User log: ${USER}"
 echo "#Network: IP ${IP} (${MAC})"
 echo "#Sudo : ${NUMBER} cmd"
