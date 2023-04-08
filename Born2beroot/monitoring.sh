6f49453d851c696342069920922fd58cfd68f5db
Your Name <you@example.com>
2023-04-08 14:23:41 +0900

    Born2beroot

--- Born2beroot/monitoring.sh
+++ Born2beroot/monitoring.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 LVM=`vgs --noheadings | awk '{if ($3 > 0) {print "yes"} else {print "no"}}'`
+
+DEVICE="enp0s8"
+IP=`ip -4 addr show ${DEVICE} | grep inet | awk -F '[ /]' '{print $6}'`
+MAC=`ip link show ${DEVICE} | grep link | awk '{print $2}'`
+
 NUMBER=$((36#`head -n 1 /var/log/sudo/seq`))
 
 echo "#LVM use: ${LVM}"
+echo "#Network: IP ${IP} (${MAC})"
 echo "#Sudo : ${NUMBER} cmd"
