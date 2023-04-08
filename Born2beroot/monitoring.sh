c87fc31cdcaa0adbcd9c722c4874b5830114fabb
Your Name <you@example.com>
2023-04-08 13:05:57 +0900

    Born2beroot

--- Born2beroot/monitoring.sh
+++ Born2beroot/monitoring.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LVM=`vgs --noheadings | awk '{if ($3 > 0) {print "yes"} else {print "no"}}'`
 NUMBER=$((36#`head -n 1 /var/log/sudo/seq`))
 
+echo "#LVM use: ${LVM}"
 echo "#Sudo : ${NUMBER} cmd"
