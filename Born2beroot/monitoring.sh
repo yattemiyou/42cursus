809454b5908daec4b3e2e5eae89a2e436fc5c350
Your Name <you@example.com>
2023-04-23 19:17:50 +0900

    Born2beroot

--- Born2beroot/monitoring.sh
+++ Born2beroot/monitoring.sh
@@ -3,6 +3,7 @@
 ARCH=`uname -a`
 PROC=`grep "physical id" /proc/cpuinfo | sort -u | wc -l`
 VPROC=`grep processor /proc/cpuinfo | wc -l`
+MEM=`free --mebi | awk '$1=="Mem:" {printf "%d/%dMB (%.2f%%)", $2-$7, $2, ($2-$7)/$2*100}'`
 CPU=`top -b -n 1 | awk -F ',' 'NR==3 {print $4}' | awk '{printf "%.1f", 100.0-$1}'`
 UPTIME=`uptime -s | awk -F ':' '{print $1 ":" $2}'`
 LVM=`vgs --noheadings | awk '{if ($3 > 0) {print "yes"} else {print "no"}}'`
@@ -20,6 +21,7 @@ NUMBER=$((36#`head -n 1 /var/log/sudo/seq`))
 echo "#Architecture: ${ARCH}"
 echo "#CPU physical : ${PROC}"
 echo "#vCPU : ${VPROC}"
+echo "#Memory Usage: ${MEM}"
 echo "#CPU load: ${CPU}%"
 echo "#Last boot: ${UPTIME}"
 echo "#LVM use: ${LVM}"
