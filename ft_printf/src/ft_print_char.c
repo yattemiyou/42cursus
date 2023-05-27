25d9d2c1ffe9069ff11b61de942c1ad68fa63a9e
Your Name <you@example.com>
2023-05-28 07:22:44 +0900

    ft_printf

--- ft_printf/src/ft_print_char.c
+++ ft_printf/src/ft_print_char.c
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_print_char.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/22 22:11:28 by anonymous         #+#    #+#             */
-/*   Updated: 2023/05/28 07:16:04 by anonymous        ###   ########.fr       */
+/*   Created: 2023/05/28 07:12:58 by anonymous         #+#    #+#             */
+/*   Updated: 2023/05/28 07:21:03 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdarg.h>
+#include <unistd.h>
 
-int		ft_printf(const char *format, ...);
-ssize_t	ft_print_char(va_list *ap);
-ssize_t	ft_print_decimal(va_list *ap);
+ssize_t	ft_print_char(va_list *ap)
+{
+	const char	c = (char)va_arg(*ap, int);
 
-#endif
+	return (write(1, &c, 1));
+}
