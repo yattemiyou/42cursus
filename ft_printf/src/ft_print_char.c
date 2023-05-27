06aa993f0ea958d852709f2e5f9049473e158c0e
Your Name <you@example.com>
2023-05-28 07:37:18 +0900

    ft_printf

--- ft_printf/src/ft_print_char.c
+++ ft_printf/src/ft_print_char.c
@@ -6,17 +6,15 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 07:12:58 by anonymous         #+#    #+#             */
-/*   Updated: 2023/05/28 07:21:03 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/05/28 07:35:22 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-#include <unistd.h>
-
 ssize_t	ft_print_char(va_list *ap)
 {
-	const char	c = (char)va_arg(*ap, int);
+	const char	value = (char)va_arg(*ap, int);
 
-	return (write(1, &c, 1));
+	return (write(1, &value, 1));
 }
--- ft_printf/src/ft_print_decimal.c
+++ ft_printf/src/ft_print_decimal.c
@@ -1,23 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_print_decimal.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/22 22:11:28 by anonymous         #+#    #+#             */
-/*   Updated: 2023/05/28 07:29:30 by anonymous        ###   ########.fr       */
+/*   Created: 2023/05/28 07:25:21 by anonymous         #+#    #+#             */
+/*   Updated: 2023/05/28 07:35:27 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdarg.h>
-# include <unistd.h>
+#include <stdio.h>
 
-int		ft_printf(const char *format, ...);
-ssize_t	ft_print_char(va_list *ap);
-ssize_t	ft_print_decimal(va_list *ap);
+ssize_t	ft_print_decimal(va_list *ap)
+{
+	const int	value = va_arg(*ap, int);
 
-#endif
+	return (printf("%d", value));
+}
