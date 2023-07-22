665c5e00c04715eed3c785e71577ceedbcd05c69
Your Name <you@example.com>
2023-07-22 11:40:26 +0900

    minitalk

--- minitalk/ft_server.c
+++ minitalk/ft_server.c
@@ -6,15 +6,26 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 10:34:50 by anonymous         #+#    #+#             */
-/*   Updated: 2023/07/22 10:38:36 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/07/22 11:39:40 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_common.h"
 
-int	main(int argc, char *argv[])
+static int	initialize(void)
 {
-	(void)argc;
-	(void)argv;
+	if (ft_print_pid(getpid()) < 0)
+		return (-1);
+	if (ft_print_string("\n") < 0)
+		return (-1);
+	return (0);
+}
+
+int	main(void)
+{
+	if (initialize() < 0)
+		return (1);
+	while (1)
+		pause();
 	return (0);
 }
