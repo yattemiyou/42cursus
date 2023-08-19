1aabf80b3b084bbc6bc1f769ba41aaa573a344b6
Your Name <you@example.com>
2023-08-20 05:51:30 +0900

    push_swap

--- push_swap/ft_chunk.c
+++ push_swap/ft_chunk.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 18:13:57 by anonymous         #+#    #+#             */
-/*   Updated: 2023/08/20 05:38:00 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/08/20 05:47:14 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,38 @@
 #include "ft_operation.h"
 #include "ft_sort.h"
 #include "ft_util.h"
+
+static int64_t	move_chunk(t_stack *src, t_stack *dest)
+{
+	int64_t	current_value;
+
+	if (src->head->next->value < dest->head->next->value)
+	{
+		current_value = src->head->next->value;
+		push_stack(src, dest, 1);
+	}
+	else
+	{
+		current_value = dest->head->next->value;
+		push_stack(dest, dest, 1);
+	}
+	rotate_stack(dest, 1);
+	return (current_value);
+}
+
+static void	gather_chunk(t_stack *src, t_stack *dest, int64_t current_value)
+{
+	t_node	*node;
+
+	node = src->head->next;
+	while (node != src->head && current_value < node->value)
+	{
+		push_stack(src, dest, 1);
+		rotate_stack(dest, 1);
+		current_value = node->value;
+		node = src->head->next;
+	}
+}
 
 void	make_chunk(t_stack *stack_a, t_stack *stack_b, int n)
 {
@@ -41,5 +73,31 @@ void	make_chunk(t_stack *stack_a, t_stack *stack_b, int n)
 		else
 			push_stack(stack_a, stack_b, 2);
 		asc ^= 0x01;
+	}
+}
+
+void	merge_chunk(t_stack *src, t_stack *dest)
+{
+	int64_t	current_value;
+	t_stack	*temp;
+
+	while (is_sorted(src) == FALSE || is_sorted(dest) == FALSE)
+	{
+		current_value = INT64_MIN;
+		while (src->len > 0 && dest->len > 0)
+		{
+			if (current_value > src->head->next->value)
+				break ;
+			if (current_value > dest->head->next->value)
+				break ;
+			current_value = move_chunk(src, dest);
+		}
+		if (src->len == 0 || current_value > src->head->next->value)
+			gather_chunk(dest, dest, current_value);
+		else if (dest->len == 0 || current_value > dest->head->next->value)
+			gather_chunk(src, dest, current_value);
+		temp = src;
+		src = dest;
+		dest = temp;
 	}
 }
--- push_swap/ft_chunk.h
+++ push_swap/ft_chunk.h
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 18:12:34 by anonymous         #+#    #+#             */
-/*   Updated: 2023/08/20 05:36:46 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/08/20 05:46:26 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 # include "ft_stack.h"
 
 void	make_chunk(t_stack *stack_a, t_stack *stack_b, int n);
+void	merge_chunk(t_stack *src, t_stack *dest);
 
 #endif
