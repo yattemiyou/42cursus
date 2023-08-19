cc614c5263fc85ba952df436859afa09e18f3c6d
Your Name <you@example.com>
2023-08-19 15:58:59 +0900

    push_swap

---
+++ push_swap/ft_operation.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_operation.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/19 15:27:59 by anonymous         #+#    #+#             */
+/*   Updated: 2023/08/19 15:58:11 by anonymous        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_operation.h"
+
+#include "ft_util.h"
+
+static void	print_operation(char *operation, char label)
+{
+	static char	label_lf[] = {'x', '\n', '\0'};
+
+	ft_print_string(STDOUT_FILENO, operation);
+	label_lf[0] = label;
+	ft_print_string(STDOUT_FILENO, label_lf);
+}
+
+void	swap_stack(t_stack *stack)
+{
+	t_node	*n0;
+	t_node	*n1;
+	t_node	*n2;
+	t_node	*n3;
+
+	if (stack->len <= 1)
+		return ;
+	print_operation("s", stack->label);
+	n0 = stack->head;
+	n1 = n0->next;
+	n2 = n1->next;
+	n3 = n2->next;
+	n0->next = n2;
+	n2->prev = n0;
+	n2->next = n1;
+	n1->prev = n2;
+	n1->next = n3;
+	n3->prev = n1;
+}
---
+++ push_swap/ft_operation.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_operation.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/19 15:25:07 by anonymous         #+#    #+#             */
+/*   Updated: 2023/08/19 15:55:55 by anonymous        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_OPERATION_H
+# define FT_OPERATION_H
+
+# include "ft_stack.h"
+
+void	swap_stack(t_stack *stack);
+
+#endif
